9d89246fc1c24acae263d2f53580f27d98a6efba
gengwenhao <gengwenhao97@126.com>
2018-08-03 00:56:35 +0800

    准备开发

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/**
+ * @github：github.com/gengwenhao
+ * @author GengWenhao
+ * @date 2018/8/3
+ * @time 0:52
+ * @Description: React Component App
+ */
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <h4>react notes</h4>
+        )
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App.jsx'
+
+
+ReactDOM.render(<App/>, document.getElementById('root'))
